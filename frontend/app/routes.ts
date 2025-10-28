1e5bb0332890dda7e74709a9b59c9970a9ef2049
Rui Weng <ruiwengc@gmail.com>
2025-10-28 17:16:45 -0400

    fix url

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("team-maker", "routes/team-maker.tsx"),
-    route("grading", "routes/grading-display.tsx")
+    route("grading-display", "routes/grading-display.tsx")
 ] satisfies RouteConfig;
